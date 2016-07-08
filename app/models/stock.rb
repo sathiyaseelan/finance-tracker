feb3805b23ce92c1ed109a20208853c6e0c1037d
sathiyaseelan <ssstarlet@gmail.com>
2016-07-08 18:08:38 +0000

    Listing of Stocks & Portfolio page completed

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ActiveRecord::Base
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+    
   def self.find_by_symbol(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
